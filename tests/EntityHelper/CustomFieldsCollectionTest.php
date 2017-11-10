fb2a36ad59011c3666f768ea882a0fc954b4a680
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-11-11 00:23:14 +0100

    [Tests] CustomFieldsCollectionTest passes

--- tests/EntityHelper/CustomFieldsCollectionTest.php
+++ tests/EntityHelper/CustomFieldsCollectionTest.php
@@ -8,6 +8,29 @@ use PHPUnit\Framework\TestCase;
 
 class CustomFieldsCollectionTest extends TestCase
 {
+    public function setUp()
+    {
+        global $kernel;
+        if (!$kernel || 'M' === get_class($kernel)[0]) { // kernel is not set or is Mocked class
+            // create mocked container in mocked kernel for CustomFieldBase
+            $config = array('testGetSet' => array('notYetExisting' => array('type' => 'Symfony\Component\Form\Extension\Core\Type\TextType')));
+            $mockContainer = $this->getMockBuilder('dummy\Container')
+                ->disableAutoload()
+                ->setMethods(array('getParameter'))
+                ->getMock();
+            $mockContainer->expects($this->any())->method('getParameter')->will($this->returnValue($config));
+            $mockKernel = $this->getMockBuilder('dummy\Kernel')
+                ->disableAutoload()
+                ->setMethods(array('getContainer'))
+                ->getMock();
+            $mockKernel
+                ->expects($this->atLeastOnce()) // remove generating $kernel when not needed anymore
+                ->method('getContainer')
+                ->will($this->returnValue($mockContainer));
+            $kernel = $mockKernel;
+        }
+    }
+
     public function testGetSet()
     {
         $cfac = new CustomFieldsCollection();
