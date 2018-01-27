17fa1f0810a0d9cf29ac107123716c3779e4673d
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2018-01-27 13:50:08 +0100

    only set ModelTransformer if it does something

--- src/Form/CustomFieldsFormService.php
+++ src/Form/CustomFieldsFormService.php
@@ -113,7 +113,7 @@ class CustomFieldsFormService
                 $form->add($name, $fieldType, $options);
             }
             // add model transformer for entity type fields
-            if (EntityMapper::isEntityField($field['type'])) {
+            if (EntityMapper::isEntityField($field['type']) && $reverseAsString) {
                 $form->get($name)->addModelTransformer(new \CubeTools\CubeCustomFieldsBundle\EntityHelper\EntityCustomFieldTransformer($this->er->getManager(), $fieldType, $reverseAsString));
             }
         }
